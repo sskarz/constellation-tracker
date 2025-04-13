d9b272ccf9321e51b987fd5cb7fe7e6b4baf6866
sskarz <sanskar1016pro@outlook.com>
2025-04-13 09:27:56 -0700

    Fixed Habit page routing issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
-import { Component } from 'react';
+import { Component } from "react";
 import Navbar from "./Navbar.jsx";
-import { Route, Routes } from 'react-router-dom';
-import Home from './pages/Home.jsx';
-import Prompt from './pages/Prompt.jsx';   
-import Details from './pages/Details.jsx'; 
-import HabitMap from './habitTracker.jsx'
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Prompt from "./pages/Prompt.jsx";
+import Details from "./pages/Details.jsx";
+import HabitMap from "./habitTracker.jsx";
 
 function App() {
- 
-  return(
+  return (
     <>
       <Navbar />
-      <div className= "container">
+      <div className="container">
         <Routes>
-          <Route path = "/home" element={<Home />}/>
-          <Route path = "/prompt" element={<Prompt />}/>
+          <Route path="/home" element={<Home />} />
+          <Route path="/prompt" element={<Prompt />} />
           <Route path="/details" element={<Details />} />
-          <Route path = "/habit" element={<HabitMap />}/>
+          <Route path="/habitTracker" element={<HabitMap />} />
         </Routes>
       </div>
     </>
--- src/habitTracker.jsx
+++ src/habitTracker.jsx
@@ -1,75 +1,75 @@
-import '../component/habitCard.tsx'
-import ConstellationTracker from '../component/habitCard.tsx';
-import '../component/star-node.tsx'
-import HabitDetails from '../component/habitDetails.tsx'; '../component/HabitDetails.tsx'
-import './index.css'
+import "../component/habitCard.tsx";
+import ConstellationTracker from "../component/habitCard.tsx";
+import "../component/star-node.tsx";
+import HabitDetails from "../component/habitDetails.tsx";
+("../component/HabitDetails.tsx");
+import "./index.css";
 
 function habitTracker() {
-
   return (
     //Out Div is the webpage itsel
-    <div  style={{
-      height: '100vh',
-      width: '100vw',
-      backgroundColor: '#0f172a',
-      overflow: 'hidden',
-      position: 'relative',
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      flexDirection: 'row',
-    }}>
+    <div
+      style={{
+        height: "100vh",
+        width: "100vw",
+        backgroundColor: "#0f172a",
+        overflow: "hidden",
+        position: "relative",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "row",
+      }}
+    >
       <div>
-       {Array.from({ length: 800 }).map((_, i) => (
+        {Array.from({ length: 800 }).map((_, i) => (
           <div
             key={`bg-star-${i}`}
             style={{
-              position: 'absolute',
-              width: '2px',
-              height: '2px',
-              backgroundColor: 'white',
-              borderRadius: '50%',
+              position: "absolute",
+              width: "2px",
+              height: "2px",
+              backgroundColor: "white",
+              borderRadius: "50%",
               opacity: 0.7,
               top: `${Math.random() * 100}%`,
               left: `${Math.random() * 100}%`,
-              animation: `twinkle ${1 + Math.random() *1}s ease-in-out infinite ${Math.random() * 3}s`,
+              animation: `twinkle ${
+                1 + Math.random() * 1
+              }s ease-in-out infinite ${Math.random() * 3}s`,
             }}
           />
         ))}
       </div>
-     
-      <div style={{
-        flex: 1,
-        paddingLeft: '1rem', 
-        paddingRight: '1rem',
-      
-      }} > 
-        <HabitDetails/>
-      </div>
-    
 
-          
-        {/*Container For Constailation*/}
-      <div style={{
-        flex: 2,
+      <div
+        style={{
+          flex: 1,
+          paddingLeft: "1rem",
+          paddingRight: "1rem",
+        }}
+      >
+        <HabitDetails />
+      </div>
+
+      {/*Container For Constailation*/}
+      <div
+        style={{
+          flex: 2,
           margin: 1,
           //border: '3px solid green'
           // border: '1px dashed white', // Uncomment if you want to see it
-        }}>
-          <ConstellationTracker days={6} />
+        }}
+      >
+        <ConstellationTracker days={6} />
       </div>
-      
-      <div style={{
-        flex:1
-      }}>
-        
-      </div>
-       
-      </div>
-    
-       
 
-   
+      <div
+        style={{
+          flex: 1,
+        }}
+      ></div>
+    </div>
   );
 }
 export default habitTracker;
